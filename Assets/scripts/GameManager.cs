6340ecc6bde50191a3674ed3dc33a507039fc817
badman0604 <serios0604@gmail.com>
2022-11-21 10:59:24 +0800

    多加分數顯示
    作業3

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -8,12 +8,13 @@ public class GameManager : MonoBehaviour
     GameObject car;              // 用來儲存汽車物件
     GameObject flag;             // 用來儲存旗子物件
     GameObject distance;         // 用來儲存UI文字物件
-
+    GameObject Score;       //用來儲存分數文字物件
     void Start() // 將場景中的物件放到已宣告的各個物件中
     {
         car = GameObject.Find("car");         
         flag = GameObject.Find("flag");
         distance = GameObject.Find("Distance");
+        Score = GameObject.Find("Score");
     }
 
     void Update()
@@ -22,5 +23,7 @@ public class GameManager : MonoBehaviour
         float length = flag.transform.position.x - car.transform.position.x;
         // 將旗子與汽車的距離顯示在UI上面
         distance.GetComponent<Text>().text = "距離目標還有 " + length.ToString("F2") + "m";
+        float ScoreNumber = 100 / length;//分數計算
+        Score.GetComponent<Text>().text = "分數 " + length.ToString("公尺") + "m";
     }
 }
